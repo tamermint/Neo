a27b30d9e84d75bd1bb9617b3c833bfa708437f7
Vivek <vivek.mitra@outlook.com>
2025-07-19 21:58:24 +1000

    remappings updated

--- test/ESFVault.t.sol
+++ test/ESFVault.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-import {Test} from "lib/forge-std/src/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {console2} from "forge-std/console2.sol";
 import {ESFVault} from "../src/ESFVault.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
